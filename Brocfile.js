ce3e42b25d3aeeb0e29768c41eb8eee642f969a0
Reza Muhammad <rezmuh@sixceed.com>
2015-07-01 15:29:32 +0700

    added bootstrap.js

--- Brocfile.js
+++ Brocfile.js
@@ -11,6 +11,7 @@ var app = new EmberApp({
 
 });
 
+app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
