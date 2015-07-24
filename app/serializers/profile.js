2b28f3b78c9d954fea805ee681f0a0972511d43a
Reza Muhammad <reza@moxy.asia>
2015-07-24 23:27:03 +0700

    remove ember-attachable in favor of ember-cli-form-data.

--- app/serializers/profile.js
+++ app/serializers/profile.js
@@ -1,7 +1,12 @@
+import Ember from 'ember';
 import DRFSerializer from './drf';
 
 export default DRFSerializer.extend({
   extract: function(store, type, payload) {
     return [payload];
-  }
+  },
+  serializeIntoHash: function(data, type, record, options) {
+    var root = Ember.String.decamelize(type.modelName);
+    data[root] = this.serialize(record, options);
+  },
 });
