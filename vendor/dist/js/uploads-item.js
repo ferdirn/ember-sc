b552e7f2c1c11df8edd51d86d043615d521a1339
oktaviardi pratama putra <oktaviardi2510@yahoo.co.id>
2015-07-28 19:06:40 +0700

    update upload multiple image

--- vendor/dist/js/uploads-item.js
+++ vendor/dist/js/uploads-item.js
@@ -133,3 +133,57 @@ function removeRowTimed(input) {
     var parent = input.parentNode.parentNode;
   document.getElementById('contenttimed').removeChild(parent);
 }
+
+window.onload = function(){
+        
+    //Check File API support
+    if(window.File && window.FileList && window.FileReader)
+    {
+        var filesInput = document.getElementById("files");
+        
+        filesInput.addEventListener("change", function(event){
+            
+            var files = event.target.files; //FileList object
+            var output = document.getElementById("result");
+            
+            for(var i = 0; i< files.length; i++)
+            {
+                var file = files[i];
+                
+                //Only pics
+                if(!file.type.match('image'))
+                  continue;
+                
+                var picReader = new FileReader();
+                
+                picReader.addEventListener("load",function(event){
+                    
+                    var picFile = event.target;
+                    
+                    var div = document.createElement("span");
+                      div.className = 'frame-thumbnail';
+                    div.innerHTML = "<img class='thumbnail-upload' src='" + picFile.result + "'" +
+                            "title='" + picFile.name + "'/>";
+                    
+                    output.insertBefore(div,null);  
+                    document.getElementById('product-image').remove(this);          
+                
+                });
+                
+                 //Read the image
+                picReader.readAsDataURL(file);
+            }                               
+           
+        });
+    }
+    else
+    {
+        console.log("Your browser does not support File API");
+    }
+}
+    
+
+
+
+
+
