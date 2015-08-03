2a3056116517f3fea46c7dc6c6a203d3b274f5fd
gilang <gilang@moxy.asia>
2015-08-03 15:27:13 +0700

    sales detail updates

--- app/routes/sales/detail.js
+++ app/routes/sales/detail.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import PageLoaderMixin from 'sellercenter/mixins/page-loader';
+import config from '../../config/environment';
 
 export default Ember.Route.extend({
-    setupController: function(controller, params) {
-        controller.set('order_number', params.order_number);
-    }
+	model: function(params) {
+		return Ember.$.getJSON(config.APP.API_HOST + '/api/salesdetail/' + params.order_number + '/');
+  },
+  setupController: function(controller, model, params) {
+  	controller.set('model', model);  }
 });
--- config/environment.js
+++ config/environment.js
@@ -78,7 +78,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'local') {
-    var HOST = 'http://api.demo.moxy.co.id';
+    var HOST = 'http://192.168.2.78:8000';
     ENV.APP.API_HOST = HOST;
     ENV['simple-auth-token']['serverTokenEndpoint'] = HOST + '/api/auth/';
     ENV['contentSecurityPolicy']['connect-src'] = "'self' " + HOST;
