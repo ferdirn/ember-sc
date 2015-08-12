d15c15ffaedcefabe765c6585d39af597a1ec6b8
Uung <avenpace@gmail.com>
2015-08-12 16:40:02 +0700

    disable not-found handler in detail page because it causing model detail page null

--- app/routes/products/detail.js
+++ app/routes/products/detail.js
@@ -3,10 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
+      /*
       var self = this;
       return this.get('store').find('product', params.id).then(function() {}, function() {
         self.transitionTo('/not-found');
       });
+      */
+     return this.get('store').find('product', params.id); 
   },
   setupController: function(controller, model) {
       controller.set('model', model);
