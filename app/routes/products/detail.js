4b82030605a701c3973c30f15bc75af9929a4fef
Ferdi Ramdhon <ferdi@moxy.asia>
2015-08-13 10:19:11 +0700

    set return value when ajax success

--- app/routes/products/detail.js
+++ app/routes/products/detail.js
@@ -4,7 +4,9 @@ export default Ember.Route.extend({
 
   model: function(params) {
       var self = this;
-      return this.get('store').find('product', params.id).then(function() {}, function() {
+      return this.get('store').find('product', params.id).then(function(data) {
+        return data;
+      }, function() {
         self.transitionTo('/not-found');
       });
   },
