141565757e1977802597f3aaa7e00cb6ef029541
Reza Muhammad <reza@moxy.asia>
2015-08-24 20:02:12 +0700

    more deprecated warnings removal

--- app/routes/products/list.js
+++ app/routes/products/list.js
@@ -4,7 +4,7 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 export default Ember.Route.extend(RouteMixin, {
   perPage: 10,
   model: function() {
-    return this.store.find('product');
+    return this.store.findAll('product');
     // this.findPaged('product', params);
   },
   setupController: function(controller, model) {
