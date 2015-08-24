f12164cc0280c7d1d3efad0d28031267be8473a3
Reza Muhammad <reza@moxy.asia>
2015-08-24 11:04:02 +0700

    remove ifCond helper as we can now use ember-truth-helpers

--- app/app.js
+++ app/app.js
@@ -13,41 +13,6 @@ App = Ember.Application.extend({
   Resolver: Resolver
 });
 
-
-/* TODO: This can be removed in favor of ember-truth-helpers.
- * One can use the following options with ember-truth-helpers:
- * {{#if (eq val1 val2)}} {{/if}}
- * {{#if (not-eq val1 val2)}} {{/if}}
- * {{#if (not val1)}} {{/if}}
- * {{#if (and val1 val2)}} {{/if}}
- * {{#if (or val1 val2)}} {{/if}}
- * {{#if (gte val1 val2)}} {{/if}}
- * {{#if (lte val1 val2)}} {{/if}}
- * {{#if (is-array val1)}} {{/if}}
- */
-
-Ember.Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-        case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-        default:
-            return options.inverse(this);
-    }
-});
 loadInitializers(App, config.modulePrefix);
 
 export default App;
