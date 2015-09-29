c612cce80832d60396881b8f44b78b4771c840b9
gilang <gilang@moxy.asia>
2015-09-29 15:39:53 +0700

    forgot password

--- app/controllers/auth/forgot-password.js
+++ app/controllers/auth/forgot-password.js
@@ -3,18 +3,33 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	fname: null,
 	isUsernameNotFound: true,
-	validUsernames: ['kambing', 'ayam', 'sapi'],
-    save: function () {
-    	var fname = this.get('fname');
-        var validUsernames = this.get('validUsernames');
-    	if (validUsernames.indexOf(fname) >= 0){
-    		this.set('isUsernameNotFound', true);
-    	}
-    	else
-    	{
-    		this.set('isUsernameNotFound', false);
-    	}
+    save: function(){
+        var fname = this.get('fname');
+        var tb1 = $("#fname").val();
+
+        $.ajax({
+          type: "POST",
+          url: 'http://localhost:8000/api/reset-password',
+          data: "value="+tb1,
+          success: function(data) {
+            alert(tb1)
+        },
+        dataType: JSON
+    });
+
     }
+
+    // save: function () {
+    // 	var fname = this.get('fname');
+    //     var validUsernames = this.get('validUsernames');
+    // 	if (validUsernames.indexOf(fname) >= 0){
+    // 		this.set('isUsernameNotFound', true);
+    // 	}
+    // 	else
+    // 	{
+    // 		this.set('isUsernameNotFound', false);
+    // 	}
+    // }
  //    fname: null,
 	// isUsernameNotFound: false,
 	// validUsernames: ['kambing', 'ayam', 'sapi'],
