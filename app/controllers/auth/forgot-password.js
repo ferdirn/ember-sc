35b1a74f25dd2ce2a99fb356dcb64d09d4753dd6
gilang <gilang@moxy.asia>
2015-09-30 13:17:59 +0700

    MOXY-332 reset password

--- app/controllers/auth/forgot-password.js
+++ app/controllers/auth/forgot-password.js
@@ -3,46 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	fname: null,
 	isUsernameNotFound: true,
-    // save: function(){
-    //     var fname = this.get('fname');
-    //     var tb1 = $("#fname").val();
-
-    //     $.ajax({
-    //       type: "POST",
-    //       url: 'http://localhost:8000/api/reset-password',
-    //       data: "value="+tb1,
-    //       success: function(data) {
-    //         alert(tb1)
-    //     },
-    //     dataType: 'JSON'
-    // });
-
-    // }
-
-    // save: function () {
-    // 	var fname = this.get('fname');
-    //     var validUsernames = this.get('validUsernames');
-    // 	if (validUsernames.indexOf(fname) >= 0){
-    // 		this.set('isUsernameNotFound', true);
-    // 	}
-    // 	else
-    // 	{
-    // 		this.set('isUsernameNotFound', false);
-    // 	}
-    // }
- //    fname: null,
-	// isUsernameNotFound: false,
-	// validUsernames: ['kambing', 'ayam', 'sapi'],
- //    save: function () {
- //    	var fname = this.get('fname');
- //    	if (validUsernames.search(fname) >= 0){
- //    		this.set('isUsernameNotFound', false);
- //    		alert(fname);
- //    	}
- //    	else
- //    	{
- //    		this.set('isUsernameNotFound', true);
- //    		alert('halo human');
- //    	}
- //    }
+    save: function(){
+        var self = this;
+        var upload = self.store.createRecord('reset-password',{
+            email: this.get('fname')
+        });
+        function onSuccess(data) {
+            // var token = data.get('token');
+            alert('Please check your email');
+        }
+        function onFailed(data) {
+            alert('Username not found');
+        }
+        upload.save().then(onSuccess, onFailed);
+    }
+   
 });
