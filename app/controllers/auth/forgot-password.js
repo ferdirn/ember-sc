e3f9cf6e6b38379d4cf6defdaec1843e096792de
gilang <gilang@moxy.asia>
2015-10-15 11:13:45 +0700

    updates forgot password blank textfield

--- app/controllers/auth/forgot-password.js
+++ app/controllers/auth/forgot-password.js
@@ -3,19 +3,25 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	fname: null,
 	isUsernameNotFound: true,
-    save: function(){
-        var self = this;
-        var upload = self.store.createRecord('reset-password',{
-            email: this.get('fname')
-        });
-        function onSuccess(data) {
-            var token = data.get('token');
-            this.set('isUsernameNotFound', true);
-        }
-        function onFailed(data) {
-            this.set('isUsernameNotFound', false);
-        }
-        upload.save().then(onSuccess, onFailed);
+  save: function(){
+    var self = this;
+    var checker = document.getElementById('fname').value; 
+    var upload = self.store.createRecord('reset-password',{
+      email: this.get('fname')
+    });
+    if (checker === ''){
+      alert('please fill your username');
+      return false;
     }
-   
+    function onSuccess(data) {
+      var token = data.get('token');
+      alert('please check your email');
+    }
+    function onFailed(data) {
+      alert('Username not found');
+      this.set('isUsernameNotFound', false);
+    }
+    upload.save().then(onSuccess, onFailed);
+  }
+
 });
