e5bf7ba617a4aece2e18149f24f22e8447722d31
Ferdi Ramdhon <ferdirn@gmail.com>
2016-04-12 14:38:39 +0700

    ORAMI-241 add retype_new_password to api params

--- app/models/change-password.js
+++ app/models/change-password.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   old_password: DS.attr(),
-  new_password: DS.attr()
+  new_password: DS.attr(),
+  retype_new_password: DS.attr()
 });
