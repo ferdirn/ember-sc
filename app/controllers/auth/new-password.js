172d9f1e7ee65e6ae9877b1c37207a68d9f2a9b2
Ferdi Ramdhon <ferdirn@gmail.com>
2016-05-04 14:09:22 +0700

    ORAMI-109 Set indentation of controller/auth/new-password.js to 2 spaces

--- app/controllers/auth/new-password.js
+++ app/controllers/auth/new-password.js
@@ -1,43 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	matchPassword: function() {
-		var password = this.get('password');
-		var repassword = this.get('repassword');
+  matchPassword: function() {
+    var password = this.get('password');
+    var repassword = this.get('repassword');
 
-		if (password !== '') {
-			if (repassword !== password) {
-				this.set('isPasswordNotMatch', true);
-			} else {
-				this.set('isPasswordNotMatch', false);
-			}
-		}
-	},
-	save: function()
-	{
-    	var self = this;
-		function getUrlVars() {
-			var vars = {};
-			window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-				vars[key] = value;
-			});
-			return vars;
-		}
-		var token = getUrlVars()["token"];
-		var email = getUrlVars()["email"];
+    if (password !== '') {
+      if (repassword !== password) {
+        this.set('isPasswordNotMatch', true);
+      } else {
+        this.set('isPasswordNotMatch', false);
+      }
+    }
+  },
+  save: function()
+  {
+      var self = this;
+    function getUrlVars() {
+      var vars = {};
+      window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+      });
+      return vars;
+    }
+    var token = getUrlVars()["token"];
+    var email = getUrlVars()["email"];
 
-		var upload = this.store.createRecord('reset-password', {
-			token: token,
-			email: email,
-			password: this.get('password'),
-			repassword: this.get('repassword')
-		});
-		function onSuccess() {
+    var upload = this.store.createRecord('reset-password', {
+      token: token,
+      email: email,
+      password: this.get('password'),
+      repassword: this.get('repassword')
+    });
+    function onSuccess() {
             self.transitionTo('auth.login');
         }
         function onFailed() {
             alert('reset password failed');
         }
-		upload.save().then(onSuccess, onFailed);
-	}
+    upload.save().then(onSuccess, onFailed);
+  }
 });
