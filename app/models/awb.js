455c3ae85e33a5d8255fb896c2cbe283bbe26f0b
harfil <harfil@bilna.com>
2016-07-20 18:48:50 +0700

    new awb structure

--- app/models/awb.js
+++ app/models/awb.js
@@ -1,11 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  vendor: DS.attr(),
   order_number: DS.attr(),
   courier: DS.attr(),
   awb: DS.attr(),
   eta_day: DS.attr(),
   manifested: DS.attr(),
-  note: DS.attr()
+  note: DS.attr(),
+  products: DS.attr()
 });
