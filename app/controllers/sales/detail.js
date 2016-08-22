159191d59070e2307762d6d4d7c9b6af69935457
harfil <harfil@bilna.com>
2016-08-22 17:21:07 +0700

    add sanitation when inputtin AWB on the fly and when sending data to backend

--- app/controllers/sales/detail.js
+++ app/controllers/sales/detail.js
@@ -2,13 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
+    sanitize: function() {
+      Ember.$('#awb').bind('keypress', function (event) {
+        var regex = new RegExp("^[a-zA-Z0-9-/\b._]+$");
+        var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+        if (!regex.test(key)) {
+          event.preventDefault();
+          return false;
+        }
+      });
+    },
     save: function() {
       var awb_model = null;
       Ember.$('.bs-example-modal-lg').modal('toggle');
       if (this.get('isEdit') === false) {
+        var sanitizedAwb = this.get('awb_model.awb').replace(/[^a-zA-Z0-9-/._]/gi, "");
         awb_model = this.store.createRecord('awb', {
           courier: this.get('awb_model.courier'),
-          awb: this.get('awb_model.awb'),
+          awb: sanitizedAwb,
           eta_day: this.get('awb_model.eta_day'),
           manifested: this.get('awb_model.manifested'),
           note: this.get('awb_model.note'),
